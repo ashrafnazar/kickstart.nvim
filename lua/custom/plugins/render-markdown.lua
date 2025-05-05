411255e8395f244e1f9a6a817413a93c282e8c19
Ashraf Nazar <ashraf.nazar@giffgaff.co.uk>
2025-05-05 12:50:32 +0100

    Update plugins

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -1,12 +1,12 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-    config = function()
-        require('render-markdown').setup({
-            completions = { lsp = { enabled = true } }
-        })
-    end
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+  config = function()
+    require('render-markdown').setup {
+      completions = { lsp = { enabled = true } },
+    }
+  end,
 }
