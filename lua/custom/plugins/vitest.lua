21a8defdd3e1cb743652160e2c4a33cbbbd11fd3
Ashraf Nazar <ashraf.nazar@giffgaff.co.uk>
2025-05-22 11:37:16 +0100

    chore: resolve merge conflict

---
+++ lua/custom/plugins/vitest.lua
@@ -0,0 +1,8 @@
+return {
+    { "marilari88/neotest-vitest" },
+    {
+        "nvim-neotest/neotest",
+        "nvim-neotest/nvim-nio",
+        opts = { adapters = { "neotest-vitest" } },
+    },
+}
