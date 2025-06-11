06dd00001bb877627baacee6bb2c4991ce1630f1
Ashraf Nazar <ashraf.nazar@giffgaff.co.uk>
2025-06-11 14:22:02 +0100

    feat: update custom plugins

--- lua/custom/plugins/extend_telescope.lua
+++ lua/custom/plugins/extend_telescope.lua
@@ -1,10 +1,10 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-        {
-            "<C-p>",
-            "<CMD>lua require('telescope.builtin').find_files()<CR>",
-            desc = "Find files",
-        },
+  'nvim-telescope/telescope.nvim',
+  keys = {
+    {
+      '<C-p>',
+      "<CMD>lua require('telescope.builtin').find_files()<CR>",
+      desc = 'Find files',
     },
+  },
 }
