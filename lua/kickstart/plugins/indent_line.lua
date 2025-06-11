f9023808ca7b7a33b07bd2a6b0f97e9e2641f763
Ashraf Nazar <ashraf.nazar@giffgaff.co.uk>
2025-06-11 14:22:02 +0100

    feat: update custom plugins

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,11 +1,30 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  opts = {
+    indent = {
+      char = '│',
+      tab_char = '│',
+    },
+    scope = {
+      enabled = true,
+      show_start = true,
+      show_end = true,
+    },
+    exclude = {
+      filetypes = {
+        'help',
+        'alpha',
+        'dashboard',
+        'neo-tree',
+        'Trouble',
+        'trouble',
+        'lazy',
+        'mason',
+        'notify',
+        'toggleterm',
+        'lazyterm',
+      },
+    },
   },
 }
