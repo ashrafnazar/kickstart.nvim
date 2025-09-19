0f1cc4abf22b82d83401d0f73ac3e22541afbbd4
Ashraf Nazar <ashraf.nazar@giffgaff.co.uk>
2025-09-19 14:34:12 +0100

    feat: update config

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -9,9 +9,23 @@ return {
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   config = function()
+    local detail = false
+
     require('oil').setup {
       keymaps = {
         ['<C-p>'] = false,
+        ['<C-s>'] = false,
+        ['gD'] = {
+          desc = 'Toggle file detail view',
+          callback = function()
+            detail = not detail
+            if detail then
+              require('oil').set_columns { 'icon', 'permissions', 'size', 'mtime' }
+            else
+              require('oil').set_columns { 'icon' }
+            end
+          end,
+        },
       },
       view_options = {
         show_hidden = true,
