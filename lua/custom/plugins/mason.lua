d6b46ffd8056e2240ddbe3be59cb74c3666604db
Ashraf Nazar <ashraf.nazar@giffgaff.co.uk>
2026-04-14 22:27:01 +0100

    feat: updates to plugins

--- lua/custom/plugins/mason.lua
+++ lua/custom/plugins/mason.lua
@@ -7,6 +7,7 @@ return {
       'gofumpt',
       'gomodifytags',
       'impl',
+      'prettierd',
     },
   },
 }
--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -3,6 +3,11 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim', -- required
     'sindrets/diffview.nvim', -- optional - Diff integration
+    'm00qek/baleia.nvim',
     'nvim-telescope/telescope.nvim', -- optional
+  },
+  cmd = 'Neogit',
+  keys = {
+    { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Show Neogit UI' },
   },
 }
